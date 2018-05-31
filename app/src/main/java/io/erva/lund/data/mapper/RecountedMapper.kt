f6f197ca3f0918b1ff51199b5321bc4156e94b17
Martirosyan, Ervin <ervin.martirosyan@sigma.software>
2018-05-31 10:11:55 +0200

    Updated comments

--- app/src/main/java/io/erva/lund/data/mapper/RecountedMapper.kt
+++ app/src/main/java/io/erva/lund/data/mapper/RecountedMapper.kt
@@ -3,6 +3,16 @@ package io.erva.lund.data.mapper
 import io.erva.lund.data.parser.BankSms
 import java.util.*
 
+/**
+ * Takes 2 sms n, n-1 and calculates balance difference
+ *
+ * 1. User has bank card currency A
+ * 2. User pays in currency B
+ * 3. Money blocked in currency B (amount A)
+ * 4. Exchange rate A to B has been changed
+ * 5. Blocked amount has written-offs after some time but it is different amount A with step 3
+ * 6. As result one more income or outcome bank operation
+ */
 class RecountedMapper : DataMapper {
 
     override fun map(parsedSmsList: List<BankSms>): List<DataItem> {
